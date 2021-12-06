ca205e3d9e08d843757c498d6eb4d3a6c7598221
Sylvain LOQUET <sloquet@e2r9p18.clusters.42paris.fr>
2021-12-06 10:54:39 +0100

    correctif2

--- sloquet.sh
+++ sloquet.sh
@@ -1,4 +1,4 @@
-#gnl test
+# gnl test
 path_test=sqtest/
 test_1=brouette.txt
 test_2=lol.txt
@@ -12,9 +12,10 @@ test_9=linew.txt
 test_10=b.txt
 test_11=bg.txt
 test_12=bigben.txt
-#
-#gnl out
-path_out=sqtest_out/
+# path_out
+path_out_42=sqtest_out_42/
+path_out_42000=sqtest_out_42000/
+# gnl_out
 out_1=out_gnl_1.txt
 out_2=out_gnl_2.txt
 out_3=out_gnl_3.txt
@@ -27,11 +28,16 @@ out_9=out_gnl_9.txt
 out_10=out_gnl_10.txt
 out_11=out_gnl_11.txt
 out_12=out_gnl_12.txt
-#valgrind
+# exec
 NAME="t42.esq"
-path_out_42000=sqtest_out_42000/
 NAME2="t42000.esq"
+# valgrind
 VFLAGS="-s -q --track-origins=yes --leak-check=full --show-leak-kinds=all"
+# directory
+rm -rf sqtest_out_42
+rm -rf sqtest_out_42000
+mkdir sqtest_out_42
+mkdir sqtest_out_42000
 #
 clear
 echo "\033[34m"
@@ -56,158 +62,152 @@ norminette sq_get_next_line.c
 norminette sq_get_next_line_utils.c
 norminette get_next_line.h
 echo
-echo "\033[34m_________________________DIFF TEST & VALGRIND_1______________________\033[0m"
+echo "\033[34m_______________DIFF TEST & VALGRIND_BUFFER_SIZE_=_42_________________\033[0m"
 # ---------- TEST 1 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_1} 1> ${path_out}${out_1} 2> ${path_out}log1
-if cmp -s ${path_out}${out_1} ${path_test}${test_1}
+valgrind ${VFLAGS} ./${NAME} ${path_test}${test_1} 1> ${path_out_42}${out_1} 2> ${path_out_42}log1
+if cmp -s ${path_out_42}${out_1} ${path_test}${test_1}
 then
-	echo "\033[34m" && diff -s ${path_out}${out_1} ${path_test}${test_1}
+	echo "\033[34m" && diff -s ${path_out_42}${out_1} ${path_test}${test_1}
 	echo "\033[32m1.OK \c\033[0m"
-	tail -n 1 ${path_out}log1
+	tail -n 1 ${path_out_42}log1
 else
 	echo "\033[31m1.KO \c\033[0m"
-	tail -n 1 ${path_out}log1
+	tail -n 1 ${path_out_42}log1
 fi
 echo
 # ---------- TEST 2 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_2} 1> ${path_out}${out_2} 2> ${path_out}log2
-if cmp -s ${path_out}${out_2} ${path_test}${test_2}
+valgrind ${VFLAGS} ./${NAME} ${path_test}${test_2} 1> ${path_out_42}${out_2} 2> ${path_out_42}log2
+if cmp -s ${path_out_42}${out_2} ${path_test}${test_2}
 then
-	echo "\033[34m" && diff -s ${path_out}${out_2} ${path_test}${test_2}
+	echo "\033[34m" && diff -s ${path_out_42}${out_2} ${path_test}${test_2}
 	echo "\033[32m2.OK \c\033[0m"
-	tail -n 1 ${path_out}log2
+	tail -n 1 ${path_out_42}log2
 else
 	echo "\033[31m2.KO \c\033[0m"
-	tail -n 1 ${path_out}log2
+	tail -n 1 ${path_out_42}log2
 fi
 echo
 # ---------- TEST 3 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_3} 1> ${path_out}${out_3} 2> ${path_out}log3
-if cmp -s ${path_out}${out_3} ${path_test}${test_3}
+valgrind ${VFLAGS} ./${NAME} ${path_test}${test_3} 1> ${path_out_42}${out_3} 2> ${path_out_42}log3
+if cmp -s ${path_out_42}${out_3} ${path_test}${test_3}
 then
-	echo "\033[34m" && diff -s ${path_out}${out_3} ${path_test}${test_3}
+	echo "\033[34m" && diff -s ${path_out_42}${out_3} ${path_test}${test_3}
 	echo "\033[32m3.OK \c\033[0m"
-	tail -n 1 ${path_out}log3
+	tail -n 1 ${path_out_42}log3
 else
 	echo "\033[31m3.KO \c\033[0m"
-	tail -n 1 ${path_out}log3
+	tail -n 1 ${path_out_42}log3
 fi
 echo
 # ---------- TEST 4 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_4} 1> ${path_out}${out_4} 2> ${path_out}log4
-if cmp -s ${path_out}${out_4} ${path_test}${test_4}
+valgrind ${VFLAGS} ./${NAME} ${path_test}${test_4} 1> ${path_out_42}${out_4} 2> ${path_out_42}log4
+if cmp -s ${path_out_42}${out_4} ${path_test}${test_4}
 then
-	echo "\033[34m" && diff -s ${path_out}${out_4} ${path_test}${test_4}
+	echo "\033[34m" && diff -s ${path_out_42}${out_4} ${path_test}${test_4}
 	echo "\033[32m4.OK \c\033[0m"
-	tail -n 1 ${path_out}log4
+	tail -n 1 ${path_out_42}log4
 else
 	echo "\033[31m4.KO \c\033[0m"
-	tail -n 1 ${path_out}log4
+	tail -n 1 ${path_out_42}log4
 fi
 echo
 # ---------- TEST 5 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_5} 1> ${path_out}${out_5} 2> ${path_out}log5
-if cmp -s ${path_out}${out_5} ${path_test}${test_5}
+valgrind ${VFLAGS} ./${NAME} ${path_test}${test_5} 1> ${path_out_42}${out_5} 2> ${path_out_42}log5
+if cmp -s ${path_out_42}${out_5} ${path_test}${test_5}
 then
-	echo "\033[34m" && diff -s ${path_out}${out_5} ${path_test}${test_5}
+	echo "\033[34m" && diff -s ${path_out_42}${out_5} ${path_test}${test_5}
 	echo "\033[32m5.OK \c\033[0m"
-	tail -n 1 ${path_out}log5
+	tail -n 1 ${path_out_42}log5
 else
 	echo "\033[31m5.KO \c\033[0m"
-	tail -n 1 ${path_out}log5
+	tail -n 1 ${path_out_42}log5
 fi
 echo
 # ---------- TEST 6 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_6} 1> ${path_out}${out_6} 2> ${path_out}log6
-if cmp -s ${path_out}${out_6} ${path_test}${test_6}
+valgrind ${VFLAGS} ./${NAME} ${path_test}${test_6} 1> ${path_out_42}${out_6} 2> ${path_out_42}log6
+if cmp -s ${path_out_42}${out_6} ${path_test}${test_6}
 then
-	echo "\033[34m" && diff -s ${path_out}${out_6} ${path_test}${test_6}
+	echo "\033[34m" && diff -s ${path_out_42}${out_6} ${path_test}${test_6}
 	echo "\033[32m6.OK \c\033[0m"
-	tail -n 1 ${path_out}log6
+	tail -n 1 ${path_out_42}log6
 else
 	echo "\033[31m6.KO \c\033[0m"
-	tail -n 1 ${path_out}log6
+	tail -n 1 ${path_out_42}log6
 fi
 echo
 # ---------- TEST 7 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_7} 1> ${path_out}${out_7} 2> ${path_out}log7
-if cmp -s ${path_out}${out_7} ${path_test}${test_7}
+valgrind ${VFLAGS} ./${NAME} ${path_test}${test_7} 1> ${path_out_42}${out_7} 2> ${path_out_42}log7
+if cmp -s ${path_out_42}${out_7} ${path_test}${test_7}
 then
-	echo "\033[34m" && diff -s ${path_out}${out_7} ${path_test}${test_7}
+	echo "\033[34m" && diff -s ${path_out_42}${out_7} ${path_test}${test_7}
 	echo "\033[32m7.OK \c\033[0m"
-	tail -n 1 ${path_out}log7
+	tail -n 1 ${path_out_42}log7
 else
 	echo "\033[31m7.KO \c\033[0m"
-	tail -n 1 ${path_out}log7
+	tail -n 1 ${path_out_42}log7
 fi
 echo
 # ---------- TEST 8 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_8} 1> ${path_out}${out_8} 2> ${path_out}log8
-if cmp -s ${path_out}${out_8} ${path_test}${test_8}
+valgrind ${VFLAGS} ./${NAME} ${path_test}${test_8} 1> ${path_out_42}${out_8} 2> ${path_out_42}log8
+if cmp -s ${path_out_42}${out_8} ${path_test}${test_8}
 then
-	echo "\033[34m" && diff -s ${path_out}${out_8} ${path_test}${test_8}
+	echo "\033[34m" && diff -s ${path_out_42}${out_8} ${path_test}${test_8}
 	echo "\033[32m8.OK \c\033[0m"
-	tail -n 1 ${path_out}log8
+	tail -n 1 ${path_out_42}log8
 else
 	echo "\033[31m8.KO \c\033[0m"
-	tail -n 1 ${path_out}log8
+	tail -n 1 ${path_out_42}log8
 fi
 echo
 # ---------- TEST 9 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_9} 1> ${path_out}${out_9} 2> ${path_out}log9
-if cmp -s ${path_out}${out_9} ${path_test}${test_9}
+valgrind ${VFLAGS} ./${NAME} ${path_test}${test_9} 1> ${path_out_42}${out_9} 2> ${path_out_42}log9
+if cmp -s ${path_out_42}${out_9} ${path_test}${test_9}
 then
-	echo "\033[34m" && diff -s ${path_out}${out_9} ${path_test}${test_9}
+	echo "\033[34m" && diff -s ${path_out_42}${out_9} ${path_test}${test_9}
 	echo "\033[32m9.OK \c\033[0m"
-	tail -n 1 ${path_out}log9
+	tail -n 1 ${path_out_42}log9
 else
 	echo "\033[31m9.KO \c\033[0m"
-	tail -n 1 ${path_out}log9
+	tail -n 1 ${path_out_42}log9
 fi
 echo
 # ---------- TEST 10 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_10} 1> ${path_out}${out_10} 2> ${path_out}log10
-if cmp -s ${path_out}${out_10} ${path_test}${test_10}
+valgrind ${VFLAGS} ./${NAME} ${path_test}${test_10} 1> ${path_out_42}${out_10} 2> ${path_out_42}log10
+if cmp -s ${path_out_42}${out_10} ${path_test}${test_10}
 then
-	echo "\033[34m" && diff -s ${path_out}${out_10} ${path_test}${test_10}
+	echo "\033[34m" && diff -s ${path_out_42}${out_10} ${path_test}${test_10}
 	echo "\033[32m10.OK \c\033[0m"
-	tail -n 1 ${path_out}log10
+	tail -n 1 ${path_out_42}log10
 else
 	echo "\033[31m10.KO \c\033[0m"
-	tail -n 1 ${path_out}log10
+	tail -n 1 ${path_out_42}log10
 fi
 echo
 # ---------- TEST 11 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_11} 1> ${path_out}${out_11} 2> ${path_out}log11
-if cmp -s ${path_out}${out_11} ${path_test}${test_11}
+valgrind ${VFLAGS} ./${NAME} ${path_test}${test_11} 1> ${path_out_42}${out_11} 2> ${path_out_42}log11
+if cmp -s ${path_out_42}${out_11} ${path_test}${test_11}
 then
-	echo "\033[34m" && diff -s ${path_out}${out_11} ${path_test}${test_11}
+	echo "\033[34m" && diff -s ${path_out_42}${out_11} ${path_test}${test_11}
 	echo "\033[32m11.OK \c\033[0m"
-	tail -n 1 ${path_out}log11
+	tail -n 1 ${path_out_42}log11
 else
 	echo "\033[31m11.KO \c\033[0m"
-	tail -n 1 ${path_out}log11
+	tail -n 1 ${path_out_42}log11
 fi
 echo
 # ---------- TEST 12 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_12} 1> ${path_out}${out_12} 2> ${path_out}log12
-if cmp -s ${path_out}${out_12} ${path_test}${test_12}
+valgrind ${VFLAGS} ./${NAME} ${path_test}${test_12} 1> ${path_out_42}${out_12} 2> ${path_out_42}log12
+if cmp -s ${path_out_42}${out_12} ${path_test}${test_12}
 then
-	echo "\033[34m" && diff -s ${path_out}${out_12} ${path_test}${test_12}
+	echo "\033[34m" && diff -s ${path_out_42}${out_12} ${path_test}${test_12}
 	echo "\033[32m12.OK \c\033[0m"
-	tail -n 1 ${path_out}log12
+	tail -n 1 ${path_out_42}log12
 else
 	echo "\033[31m12.KO \c\033[0m"
-	tail -n 1 ${path_out}log12
+	tail -n 1 ${path_out_42}log12
 fi
 echo
-echo
-rm -f get_next_line.h
-rm -f sq_get_next_line.c
-rm -f sq_get_next_line_utils.c
-rm -f ${NAME}
-
-echo "\033[34m_________________________DIFF TEST & VALGRIND_2______________________\033[0m"
+echo "\033[34m______________DIFF TEST & VALGRIND_BUFFER_SIZE_=_42000_______________\033[0m"
 # ---------- TEST 1 ----------
 valgrind ${VFLAGS} ./${NAME2} ${path_test}${test_1} 1> ${path_out_42000}${out_1} 2> ${path_out_42000}log1
 if cmp -s ${path_out_42000}${out_1} ${path_test}${test_1}
@@ -233,7 +233,7 @@ else
 fi
 echo
 # ---------- TEST 3 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_3} 1> ${path_out_42000}${out_3} 2> ${path_out_42000}log3
+valgrind ${VFLAGS} ./${NAME2} ${path_test}${test_3} 1> ${path_out_42000}${out_3} 2> ${path_out_42000}log3
 if cmp -s ${path_out_42000}${out_3} ${path_test}${test_3}
 then
 	echo "\033[34m" && diff -s ${path_out_42000}${out_3} ${path_test}${test_3}
@@ -245,7 +245,7 @@ else
 fi
 echo
 # ---------- TEST 4 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_4} 1> ${path_out_42000}${out_4} 2> ${path_out_42000}log4
+valgrind ${VFLAGS} ./${NAME2} ${path_test}${test_4} 1> ${path_out_42000}${out_4} 2> ${path_out_42000}log4
 if cmp -s ${path_out_42000}${out_4} ${path_test}${test_4}
 then
 	echo "\033[34m" && diff -s ${path_out_42000}${out_4} ${path_test}${test_4}
@@ -257,7 +257,7 @@ else
 fi
 echo
 # ---------- TEST 5 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_5} 1> ${path_out_42000}${out_5} 2> ${path_out_42000}log5
+valgrind ${VFLAGS} ./${NAME2} ${path_test}${test_5} 1> ${path_out_42000}${out_5} 2> ${path_out_42000}log5
 if cmp -s ${path_out_42000}${out_5} ${path_test}${test_5}
 then
 	echo "\033[34m" && diff -s ${path_out_42000}${out_5} ${path_test}${test_5}
@@ -269,7 +269,7 @@ else
 fi
 echo
 # ---------- TEST 6 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_6} 1> ${path_out_42000}${out_6} 2> ${path_out_42000}log6
+valgrind ${VFLAGS} ./${NAME2} ${path_test}${test_6} 1> ${path_out_42000}${out_6} 2> ${path_out_42000}log6
 if cmp -s ${path_out_42000}${out_6} ${path_test}${test_6}
 then
 	echo "\033[34m" && diff -s ${path_out_42000}${out_6} ${path_test}${test_6}
@@ -281,7 +281,7 @@ else
 fi
 echo
 # ---------- TEST 7 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_7} 1> ${path_out_42000}${out_7} 2> ${path_out_42000}log7
+valgrind ${VFLAGS} ./${NAME2} ${path_test}${test_7} 1> ${path_out_42000}${out_7} 2> ${path_out_42000}log7
 if cmp -s ${path_out_42000}${out_7} ${path_test}${test_7}
 then
 	echo "\033[34m" && diff -s ${path_out_42000}${out_7} ${path_test}${test_7}
@@ -293,7 +293,7 @@ else
 fi
 echo
 # ---------- TEST 8 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_8} 1> ${path_out_42000}${out_8} 2> ${path_out_42000}log8
+valgrind ${VFLAGS} ./${NAME2} ${path_test}${test_8} 1> ${path_out_42000}${out_8} 2> ${path_out_42000}log8
 if cmp -s ${path_out_42000}${out_8} ${path_test}${test_8}
 then
 	echo "\033[34m" && diff -s ${path_out_42000}${out_8} ${path_test}${test_8}
@@ -305,7 +305,7 @@ else
 fi
 echo
 # ---------- TEST 9 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_9} 1> ${path_out_42000}${out_9} 2> ${path_out_42000}log9
+valgrind ${VFLAGS} ./${NAME2} ${path_test}${test_9} 1> ${path_out_42000}${out_9} 2> ${path_out_42000}log9
 if cmp -s ${path_out_42000}${out_9} ${path_test}${test_9}
 then
 	echo "\033[34m" && diff -s ${path_out_42000}${out_9} ${path_test}${test_9}
@@ -317,7 +317,7 @@ else
 fi
 echo
 # ---------- TEST 10 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_10} 1> ${path_out_42000}${out_10} 2> ${path_out_42000}log10
+valgrind ${VFLAGS} ./${NAME2} ${path_test}${test_10} 1> ${path_out_42000}${out_10} 2> ${path_out_42000}log10
 if cmp -s ${path_out_42000}${out_10} ${path_test}${test_10}
 then
 	echo "\033[34m" && diff -s ${path_out_42000}${out_10} ${path_test}${test_10}
@@ -329,7 +329,7 @@ else
 fi
 echo
 # ---------- TEST 11 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_11} 1> ${path_out_42000}${out_11} 2> ${path_out_42000}log11
+valgrind ${VFLAGS} ./${NAME2} ${path_test}${test_11} 1> ${path_out_42000}${out_11} 2> ${path_out_42000}log11
 if cmp -s ${path_out_42000}${out_11} ${path_test}${test_11}
 then
 	echo "\033[34m" && diff -s ${path_out_42000}${out_11} ${path_test}${test_11}
@@ -341,7 +341,7 @@ else
 fi
 echo
 # ---------- TEST 12 ----------
-valgrind ${VFLAGS} ./${NAME} ${path_test}${test_12} 1> ${path_out_42000}${out_12} 2> ${path_out_42000}log12
+valgrind ${VFLAGS} ./${NAME2} ${path_test}${test_12} 1> ${path_out_42000}${out_12} 2> ${path_out_42000}log12
 if cmp -s ${path_out_42000}${out_12} ${path_test}${test_12}
 then
 	echo "\033[34m" && diff -s ${path_out_42000}${out_12} ${path_test}${test_12}
@@ -352,9 +352,8 @@ else
 	tail -n 1 ${path_out_42000}log12
 fi
 echo
-echo
+# end
 rm -f get_next_line.h
 rm -f sq_get_next_line.c
 rm -f sq_get_next_line_utils.c
-rm -f ${NAME2}
-
+rm -f *.esq
